7ec03d0d5414aab74eac731179369a24ab9e2279
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-02-26 15:25:03 +0700

    untuk commit

--- PertemuanPertamaKelasB_/PertemuanPertamaKelasB_.cpp
+++ PertemuanPertamaKelasB_/PertemuanPertamaKelasB_.cpp
@@ -1,7 +1,22 @@
 
 #include <iostream>
+using namespace std;
 
 int main()
-{
-    std::cout << "Hello World!\n";
-}
+{//begin
+    //numeric nPanjang, nLebar,nLuas
+    int nPanjang, nLebar, nLuas;
+    //display 'Masukan Panjang= '
+    cout << "Masukan Panjang = ";
+    //accept nPanjang
+    cin >> nPanjang;
+    //display 'Masukan Lebar = '
+    cout << "Masukan Lebar = ";
+    //accpet nLebar
+    cin >> nLebar;
+    //compute nLuas = nPanjang * nLebar
+    nLuas = nPanjang * nLebar;
+    // display 'Luasnya adalah = ' +nLuas
+    cout << "Luasnya adalah = " << nLuas << endl;
+
+}//end
